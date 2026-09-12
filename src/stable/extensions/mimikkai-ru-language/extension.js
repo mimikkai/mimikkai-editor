8baf8dc1eec1d5f89d8e2a50ed9372524cfdbba3
dealenx <dealenx@gmail.com>
2026-09-12 15:43:26 +0700

    feat(ui): hide Source Control and Run and Debug icons by default
    New product.json 'hiddenViewContainers' (consumed via a small
    paneCompositeBar patch): containers listed there are hidden from the
    activity bar by default but remain reachable via keybindings/commands
    (Ctrl+Shift+G, Ctrl+Shift+D). Default hides workbench.view.scm and
    workbench.view.debug.

--- src/stable/extensions/mimikkai-ru-language/extension.js
+++ src/stable/extensions/mimikkai-ru-language/extension.js
@@ -7,31 +7,31 @@ const LANG_PACK_ID = 'MS-CEINTL.vscode-language-pack-ru';
 const LOCALE = 'ru';
 
 function activate(context) {
-	const vscode = require('vscode');
-	context.subscriptions.push(vscode.commands.registerCommand('mimikkai.lang.installRu', () => {
-		vscode.commands.executeCommand('workbench.extensions.installExtension', LANG_PACK_ID)
-			.then(
-				() => vscode.window.showInformationMessage('Russian language pack installed. Restart MimikkAI Editor to apply.'),
-				(err) => vscode.window.showErrorMessage(`Russian language pack install failed: ${err && err.message ? err.message : err}`)
-			);
-	}));
+  const vscode = require('vscode');
+  context.subscriptions.push(vscode.commands.registerCommand('mimikkai.lang.installRu', () => {
+  vscode.commands.executeCommand('workbench.extensions.installExtension', LANG_PACK_ID)
+  .then(
+  () => vscode.window.showInformationMessage('Russian language pack installed. Restart MimikkAI Editor to apply.'),
+  (err) => vscode.window.showErrorMessage(`Russian language pack install failed: ${err && err.message ? err.message : err}`)
+  );
+  }));
 
-	setTimeout(() => {
-		let installed = false;
-		try {
-			installed = Boolean(vscode.extensions.getExtension(LANG_PACK_ID));
-		} catch (e) { installed = true; }
-		if (!installed) {
-			try {
-				vscode.commands.executeCommand('workbench.action.configureLocale', LOCALE);
-			} catch (e) { /* ignore */ }
-			vscode.commands.executeCommand('workbench.extensions.installExtension', LANG_PACK_ID)
-				.then(
-					() => vscode.window.showInformationMessage('Russian language pack installed. Restart MimikkAI Editor to switch the interface to Russian.'),
-					() => vscode.window.setStatusBarMessage('Russian language pack is not installed yet (MimikkAI: Install Russian Language Pack).', 10000)
-				);
-		}
-	}, 3000);
+  setTimeout(() => {
+  let installed = false;
+  try {
+  installed = Boolean(vscode.extensions.getExtension(LANG_PACK_ID));
+  } catch (e) { installed = true; }
+  if (!installed) {
+  try {
+  vscode.commands.executeCommand('workbench.action.configureLocale', LOCALE);
+  } catch (e) { /* ignore */ }
+  vscode.commands.executeCommand('workbench.extensions.installExtension', LANG_PACK_ID)
+  .then(
+  () => vscode.window.showInformationMessage('Russian language pack installed. Restart MimikkAI Editor to switch the interface to Russian.'),
+  () => vscode.window.setStatusBarMessage('Russian language pack is not installed yet (MimikkAI: Install Russian Language Pack).', 10000)
+  );
+  }
+  }, 3000);
 }
 
 function deactivate() { }
